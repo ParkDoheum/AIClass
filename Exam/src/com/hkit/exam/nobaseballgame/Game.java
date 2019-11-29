33d8519afd45fd730a33338ca5f9c8c297956cf9
epata94 <hklee0724@gmail.com>
2019-11-29 14:08:18 +0900

    중복되지 않는 랜덤값 while

--- Exam/src/com/hkit/exam/nobaseballgame/Game.java
+++ Exam/src/com/hkit/exam/nobaseballgame/Game.java
@@ -10,12 +10,31 @@ public class Game {
 	}
 	
 	public void start() {
-		setRandoms();
+		setRandoms2();
 		
 		for(int i : rArray) {
 			System.out.println(i);
 		}
 	}
+	
+	public void setRandoms2() {
+		int len = rArray.length;
+		int cnt = 0;
+		while(cnt < len) {
+			int r = (int)(Math.random() * 9) + 1;
+			
+			for(int i=0; i<len; i++) {
+				if(rArray[i] == 0) {
+					rArray[i] = r;
+					cnt++;
+					break;
+				} else if(rArray[i] == r) {
+					break;
+				}
+			}
+		}
+	}
+	
 	
 	public void setRandoms() { //1~9사이 랜덤값을 중복 금지!
 		for(int i=0; i<rArray.length; i++) { //몇 칸이 들어갔는지
