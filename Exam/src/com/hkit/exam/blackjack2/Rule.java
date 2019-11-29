6d36a9490ac0e8376ce8155cff0675950cb39f6f
epata94 <hklee0724@gmail.com>
2019-11-29 11:48:59 +0900

    Rule 완성

--- Exam/src/com/hkit/exam/blackjack2/Rule.java
+++ Exam/src/com/hkit/exam/blackjack2/Rule.java
@@ -2,6 +2,36 @@ package com.hkit.exam.blackjack2;
 
 public class Rule {
 	public static void whoIsWin(Gamer g1, Gamer g2) {
-		System.out.println(g2.NAME + " 가 이기셨습니다");
+		int g1Sum = g1.myCardNoSum();
+		int g2Sum = g2.myCardNoSum();
+		
+		String winNm = null;
+		if(g1Sum == g2Sum || (g1Sum > 21 && g2Sum > 21)) {
+			System.out.println("비겻습니다.");			
+		} else {
+			if(g1Sum > 21) {
+				winNm = g2.NAME;
+			} else if(g2Sum > 21){
+				winNm = g1.NAME;
+			} else {
+				if(g1Sum > g2Sum) {
+					winNm = g1.NAME;	
+				} else {
+					winNm = g2.NAME;
+				}
+			}
+			System.out.println();
+			System.out.printf("%s가 이겼습니다\n", winNm);
+		}	
+		g1.openCards();
+		g2.openCards();
 	}
 }
+
+
+
+
+
+
+
+
