14f8a7dd3b05a05353e6378166f40dd24a528f3b
epata94 <hklee0724@gmail.com>
2019-11-29 10:44:55 +0900

    게이머 카드 더 받기

--- Exam/src/com/hkit/exam/blackjack2/CardTest.java
+++ Exam/src/com/hkit/exam/blackjack2/CardTest.java
@@ -1,20 +1,49 @@
 package com.hkit.exam.blackjack2;
 
+import java.util.Scanner;
+
 public class CardTest {
 
 	public static void main(String[] args) {
 		CardDeck cd = new CardDeck();
 		Gamer gamer = new Gamer("Gamer");
 		Dealer dealer = new Dealer("Dealer");
+		Scanner scan = new Scanner(System.in);
 		
 		for(int i=0; i<2; i++) {
 			dealer.receiveCard(cd.pick());
 			gamer.receiveCard(cd.pick());	
 		}		
-		dealer.moreCard(cd);
+		dealer.moreCard(cd);		
 		
-		gamer.openCards();
+		String answer;
+		while(true) {
+			gamer.openCards();
+			System.out.print("카드를 더 받으시겠습니까? (y, n) : ");
+			answer = scan.next();
+			
+			if(answer.equals("n")) {
+				break;
+			}
+			gamer.receiveCard(cd.pick());
+		}
+		scan.close();
+		
 		dealer.openCards();
+		gamer.openCards();
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
+
+
+
+
+
+
